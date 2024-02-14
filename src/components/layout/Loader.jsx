a392673978d904afdd23ea7f2c9fce13a10a3ffe
devendragoswami1 <goswamigoswamidevendra@gmail.com>
2024-02-14 21:23:06 +0530

    laoder component updated

--- src/components/layout/Loader.jsx
+++ src/components/layout/Loader.jsx
@@ -1,10 +1,11 @@
 
 const Loader = () => {
   return (
-    <div className="w-12 h-6 relative loader">
-      <div className="w-2 h-3 absolute -left-4 animate-ping"></div>
-      <div className="absolute inset-0 rounded-full animate-spin"></div>
-    </div>
+      <>
+      <div className="flex justify-center items-center h-[80vh]">
+      <div className="rounded-md h-12 w-12 border-4 border-t-4 border-blue-500 animate-spin"></div>
+      </div>
+      </>
   );
 };
 
